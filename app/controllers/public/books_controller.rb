68e2dae847814a680ac0fa4eda98c01409e0ad66
hiiro4 <subhouse.indog@gmail.com>
2022-08-18 13:06:28 +0000

    [modify]

--- app/controllers/public/books_controller.rb
+++ app/controllers/public/books_controller.rb
@@ -1,12 +1,27 @@
 class Public::BooksController < ApplicationController
 
   def index
-    @bookshelves = Bookshelf.where(user_id:current_user.id)
+    #ジャンル一覧取得
+    #roots = RakutenWebService::Books::Genre.root (bookapiだがbook以外も取得)
+    #byebug
+    #roots.children.each do |child|
+    #byebug
+    # @put = child.booksGenreName   取得不能(名称は合っている)
+    #end
+    #byebug
     #本の検索
+    @bookshelves = Bookshelf.where(user_id:current_user.id)
     if params[:keyword].present? #入力されているか
       if params[:book_category] == "title" #検索方法の選択
-        @books = RakutenWebService::Books::Book.search(title: params[:keyword])
-        @hit = @books.count
+        books = RakutenWebService::Books::Book.search(title: params[:keyword])
+        @hit = books.count
+        @page = Kaminari.paginate_array([books], total_count: @hit).page(params[:page]).per(10)
+        if params[:page].nil?
+          now_page = 1
+        else
+          now_page = params[:page]
+        end
+        @books = RakutenWebService::Books::Book.search(title: params[:keyword],page: now_page,hits: 10)
       elsif params[:book_category] == "author"
         @books = RakutenWebService::Books::Book.search(author: params[:keyword])
         @hit = @books.count
@@ -18,18 +33,33 @@ class Public::BooksController < ApplicationController
     if params[:review_range] == "all"
       @reviews = Review.all #全てのレビューから検索
       @hoge = 1
-      if  params[:review_type] == "favorite"
-        @reviews = Review.includes(:favorite_users).sort {|a,b| b.favorite_users.size <=> a.favorite_users.size}
-      elsif params[:review_type] == "new"
-        @reviews = Review.all.order(created_at: :desc)
-      end
+      @page = @reviews.page(params[:page])
+        if  params[:review_type] == "favorite"
+          @reviews = Review.includes(:favorite_users).sort {|a,b| b.favorite_users.size <=> a.favorite_users.size}
+        elsif params[:review_type] == "new"
+          @reviews = Review.all.order(created_at: :desc)
+        end
     elsif params[:review_range] == "my"
       @hoge = 1
       @reviews = Review.where(user_id:current_user.id)    #自身のレビューから検索
-      if  params[:review_type] == "favorite"
-        @reviews = @reviews.includes(:favorite_users).sort {|a,b| b.favorite_users.size <=> a.favorite_users.size}
-      elsif params[:review_type] == "new"
-        @reviews = Review.all.order(created_at: :desc)
+        if  params[:review_type] == "favorite"
+          @reviews = @reviews.includes(:favorite_users).sort {|a,b| b.favorite_users.size <=> a.favorite_users.size}
+        elsif params[:review_type] == "new"
+          @reviews = Review.all.order(created_at: :desc)
+        end
+    elsif params[:review_range] == "follow"
+      @hoge = 1
+      if Relationship.where(be_followed_id:current_user.id).present?#フォロワーを取得、存在を判定
+         follower = Relationship.where(be_followed_id:current_user.id).pluck(:following_id)
+         #byebug
+         @reviews = Review.where(user_id:follower)#フォロワーのレビューを取得
+        if  params[:review_type] == "favorite"
+          @reviews = @reviews.includes(:favorite_users).sort {|a,b| b.favorite_users.size <=> a.favorite_users.size}
+        elsif params[:review_type] == "new"
+          @reviews = Review.all.order(created_at: :desc)
+        end
+      else
+        @judge = フォロワーがいません
       end
     else
        @reviews = Review.all.order(created_at: :desc)
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,12 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
+  # ゲストログイン
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲスト"
+    end
+  end
+
 end
